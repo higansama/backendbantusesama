1548d1c81938e77dc52a5d8ec5601fa3280efe94
Arif Nashrulloh <nashrulloh1811@gmail.com>
2020-07-24 08:43:13 +0700

    up

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"backend_skripsi/router"
+	"net/http"
+	"os"
 
 	"github.com/bandros/framework"
 )
@@ -12,5 +14,10 @@ func main() {
 
 	r := framework.Begin
 	router.Router(r)
-	framework.Run()
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	http.ListenAndServe(":"+port, nil)
+	// framework.Run()
 }
